5439b49b09c426337f3ec289d79f3d98936b1018
bCalabrese <calabresebrandon3@gmail.com>
2019-04-23 13:48:06 -0500

    Added testing utility functions

---
+++ Gleam/TestUtils.cpp
@@ -0,0 +1,29 @@
+
+#include "TestUtils.h"
+
+void _print(glm::vec3 v) {
+	printf("<%f, %f, %f>\n", v.x, v.y, v.z);
+}
+
+void _print(glm::vec4 v) {
+	printf("<%f, %f, %f, %f>\n", v.x, v.y, v.z, v.w);
+}
+
+void _print(glm::mat4 m) {
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		cout << '|';
+		for (int j = 0; j < 4; j++) {
+			if (j == 0)
+				cout << std::setw(10) << m[i][j];
+			else
+				cout << ", " << std::setw(12) << m[i][j];
+		}
+		cout << "|\n";
+	}
+	cout << endl;
+}
+
+void _print(glm::quat q) {
+	printf("<%f, %f, %f, %f>\n", q.x, q.y, q.z, q.w);
+}
---
+++ Gleam/TestUtils.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/glm.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+#include <iostream>
+#include <iomanip>
+#include <assert.h>
+
+#include "Transform.h"
+
+using std::cout;
+using std::endl;
+
+void _print(glm::vec3 v);
+void _print(glm::vec4 v);
+void _print(glm::mat4 m);
+void _print(glm::quat q);
+
+void testTransforms();
